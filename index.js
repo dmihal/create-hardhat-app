02d1e8d593dbd48318dc070c0f54add451313363
David Mihal <david@dmihal.com>
2020-12-26 23:30:16 +0000

    Generate folder from script

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 #!/usr/bin/env node
-const project_creation = require("node_modules/hardhat/internal/cli/project-creation");
+const project_creation = require("hardhat/internal/cli/project-creation");
+const fs = require('fs');
+
+if (process.argv.length <= 2) {
+  console.error('Error: must pass project directory name:');
+  console.log('    yarn create hardhat-app <project-name>\n');
+  process.exit(1);
+}
+
+const dir = process.cwd() + '/' + process.argv[2];
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir);
+}
+process.chdir(dir);
+
+fs.writeFileSync('yarn.lock', '');
 
 project_creation.createProject()
   .then(() => process.exit(process.exitCode))
